cb605c7f73f2a462241f188e4729603ed03e59f9
BrandonDonaldson4 <Donaldson_ba@yahoo.com>
2026-01-09 01:52:49 -0500

    Remove rows with missing values

--- movie_predictor.py
+++ movie_predictor.py
@@ -5,3 +5,12 @@ movies = pd.read_csv("Movie Success Predictor/movies.csv")
 print("Shape:", movies.shape)
 print("\nColumns:")
 print(movies.columns)
+
+# Check for missing values
+print("\nMissing values:")
+print(movies.isnull().sum())
+
+#Drop rows with missing values
+df = movies.dropna(subset=["overview"])
+print(df.isnull().sum())
+
